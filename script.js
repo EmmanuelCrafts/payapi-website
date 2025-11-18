5c1fbfc13dae6326be942985b43568ddffbbfdb3
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-11-18 22:52:19 +0300

    feat(nav): add desktop/tablet nav links and update layout for responsive navigation

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const menu = document.getElementById('menu');
 const closeBtn = document.getElementById('close-btn');
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.add('menu-hidden');
+    menu.classList.add('menu-slide');
 });
 
 closeBtn.addEventListener("click", ()=> {
-    menu.classList.remove('menu-hidden');  
+    menu.classList.remove('menu-slide');  
 }) 
