986691c8634076879cd4fbdb7f36bb75eb4eac68
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-11-18 13:44:54 +0300

    feat(nav): open and close side menu on menu icon click

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 const closeBtn = document.getElementById('close-btn');
 
+menuBtn.addEventListener('click', () => {
+    menu.classList.add('menu-hidden');
+});
+
+closeBtn.addEventListener("click", ()=> {
+    menu.classList.remove('menu-hidden');  
+}) 
