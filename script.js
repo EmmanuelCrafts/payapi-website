efc6ea58f8f1d716e39f0e77f5e01818bb873aa5
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-11-21 10:23:04 +0300

    fix(form-validation): add .error to parent div and style label/input on errors

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ closeBtn.addEventListener("click", ()=> {
 // Form Validation
 const form = document.getElementById('contact-form');
 const { fullName, email, companyName, title, message } = form.elements;
+
+
+// error message elements
 const nameError = document.getElementById('name-error');
 const emailError = document.getElementById('email-error');
 const companyNameError = document.getElementById('companyName-error');
@@ -31,60 +34,72 @@ form.addEventListener('submit', (e) => {
     // name validation
     if (nameValue.length === 0) {
         nameError.textContent = "This field can't be empty";
+        fullName.parentElement.classList.add('error');
     } 
 
       else if(nameValue.length < 3) {
         nameError.textContent = 'name must be at least 3 characters long.';
+        fullName.parentElement.classList.add('error');
     } 
 
       else {
         nameError.textContent = '';
+        fullName.parentElement.classList.remove('error');
     }
    
     // email validation
     if (emailValue.length === 0) {
         emailError.textContent = "This field can't be empty";
+        email.parentElement.classList.add('error');
     } 
     
       else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue)) {
         emailError.textContent = 'Please enter a valid email address.';
+        email.parentElement.classList.add('error');
     } 
       else {
         emailError.textContent = '';
+        email.parentElement.classList.remove('error');
     }
 
     // company name validation
     if (companyNameValue.length === 0) {
         companyNameError.textContent = "This field can't be empty";
+        companyName.parentElement.classList.add('error');
 
     } else if(companyNameValue.length < 3) {
         companyNameError.textContent = 'Company name must be at least 3 characters long.';
-
+        companyName.parentElement.classList.add('error');
     } else {    
         companyNameError.textContent = '';
+        companyName.parentElement.classList.remove('error');
     }
 
     // title validation
     if (titleValue.length === 0) {
         titleError.textContent = "This field can't be empty";
-
+        title.parentElement.classList.add('error');
     } else if(titleValue.length < 5) {
         titleError.textContent = 'Title must be at least 5 characters long.';
-
+        title.parentElement.classList.add('error');
     } else {
         titleError.textContent = '';
+        title.parentElement.classList.remove('error');
     }
 
     // message validation
     if (messageValue.length === 0) {
         messageError.textContent = "This field can't be empty";
-
+        message.parentElement.classList.add('error');   
     } else if (messageValue.length < 10) {
         messageError.textContent = 'Message must be at least 10 characters long.';
+        message.parentElement.classList.add('error');
     } else {
         messageError.textContent = '';
+        message.parentElement.classList.remove('error');
     }
 
+    
     // Submit the form if all fields are valid
     if (
         nameError.textContent === '' &&
